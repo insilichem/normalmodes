b799355128a76e4abef034edff0a3ae428a29139
Jordi Guasp <jordi.guasp@e-campus.uab.cat>
2016-02-23 10:43:22 +0100

    issue #1 solved, addh supported now

--- CalcNormalModes.py
+++ CalcNormalModes.py
@@ -36,37 +36,70 @@ def calc_normal_modes(mol, coarse_grain=None, n_algorithm=None):
             moldy = coarse_grain(moldy, n_algorithm)
         else:
             moldy = coarse_grain(moldy)
-        modes = prody.RTB('RTB for {} using algorithm {}'.format(moldy.getTitle, 1))
+        modes = prody.RTB('normal modes for {} using algorithm {}'.format(moldy.getTitle(), 1))
         modes.buildHessian(moldy.getCoords(), moldy.getBetas())
         modes.calcModes()
     else:
-        modes = prody.ANM('ANM for {}'.format(moldy.getTitle))
+        modes = prody.ANM('normal modes for {}'.format(moldy.getTitle()))
         modes.buildHessian(moldy)
         modes.calcModes()
     return modes
 
 
 def chimera2prody(mol):
+    """
+    Function that transforms a chimera molecule into a prody atom group
+
+    Parameters
+    ----------
+    mol: chimera.Molecule
+
+    Returns
+    -------
+    moldy: prody.AtomGroup()
+    """
     moldy = prody.AtomGroup()
     try:
-        moldy.setCoords(numpy.array([tuple(atm.coord()) for atm in mol.atoms], dtype=float))
-        moldy.setElements([atm.element.name for atm in mol.atoms])
-        moldy.setNames([atm.name for atm in mol.atoms])
-        moldy.setResnums([atm.residue.id.position for atm in mol.atoms])
-        moldy.setChids([atm.residue.id.chainId for atm in mol.atoms])
-        moldy.setBetas([atm.bfactor for atm in mol.atoms])
-        moldy.setMasses([atm.element.mass for atm in mol.atoms])
-        #moldy.setBonds([[bond.atoms[0].coordIndex, bond.atoms[1].coordIndex]for bond in mol.bonds])
-        moldy.setTitle(str(mol.name))
+        # moldy.setCoords(numpy.array([tuple(atm.coord()) for atm in mol.atoms], dtype=float))
+        # moldy.setElements([atm.element.name for atm in mol.atoms])
+        # moldy.setNames([atm.name for atm in mol.atoms])
+        # moldy.setResnums([atm.residue.id.position for atm in mol.atoms])
+        # moldy.setChids([atm.residue.id.chainId for atm in mol.atoms])
+        # moldy.setBetas([atm.bfactor for atm in mol.atoms])
+        # moldy.setMasses([atm.element.mass for atm in mol.atoms])
+        # # moldy.setBonds([[bond.atoms[0].coordIndex, bond.atoms[1].coordIndex]for bond in mol.bonds])
+        # moldy.setTitle(str(mol.name))
+
         # n = len(mol.atoms)
-        # coords, elements, names, resnums, masses = numpy.zeros(n), [], [], [], []
-        # for atm in mol.atoms:
-        #     coords.append(tuple(atm.coord()))#array documentation
-        #     elements.append(atm.element.name)
-        #     names.append(atm.name)
-        #     resnums.append(atm.residue.id.position)
-        #     masses.append(atm.element.mass)
-        # moldy.setCoords(coords)
+        # coords = numpy.zeros(n)
+        coords, elements, names, resnums, chids, betas, masses, title = [
+        ], [], [], [], [], [], [], []
+        d, e = {}, {}
+
+        for i, atm in enumerate(mol.atoms):
+            d[i] = atm.coordIndex
+            e[atm.coordIndex] = i
+
+            coords.append(tuple(atm.coord()))  # array documentation to improve
+            elements.append(atm.element.name)
+            names.append(atm.name)
+            resnums.append(atm.residue.id.position)
+            chids.append(atm.residue.id.chainId)
+            masses.append(atm.element.mass)
+            betas.append(atm.bfactor)
+
+        moldy.setCoords(coords)
+        moldy.setElements(elements)
+        moldy.setNames(names)
+        moldy.setResnums(resnums)
+        moldy.setChids(chids)
+        moldy.setBetas(betas)
+        moldy.setMasses(masses)
+
+        moldy.setBonds([[e[bond.atoms[0].coordIndex], e[bond.atoms[1].coordIndex]]
+                        for bond in mol.bonds])
+
+        moldy.setTitle(mol.name)
 
     except AttributeError:
         raise AttributeError('mol must be a chimera.Molecule')
